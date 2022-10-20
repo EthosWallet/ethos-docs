f6f95fb1ccb12e6634585238af8f10d429e86c1a
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-10-20 16:16:54 -0500

    Update logo

--- src/components/TryItOut.jsx
+++ src/components/TryItOut.jsx
@@ -17,7 +17,7 @@ const TryItOut = () => {
                 </div>
             </div>
             <div className='grid place-items-center h-60'>
-                <SignInButton className='rounded-full bg-purple-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500' />
+                <SignInButton className='rounded-full bg-[#6D28D9] py-2 px-4 text-sm font-semibold text-white' />
             </div>
         </div>
     );
