fb4cf585de0cf12808dde3b63ca8b7bf773d0522
Jared Cosulich <jared.cosulich@gmail.com>
2023-04-05 14:30:08 -0400

    removing console.log

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,6 @@ export function Hero() {
     preferredWallets: ['Ethos Wallet'],
     network: "https://fullnode.devnet.sui.io"
   };
-  console.log("ethosConfiguration", ethosConfiguration)
 
   return (
     <EthosConnectProvider
